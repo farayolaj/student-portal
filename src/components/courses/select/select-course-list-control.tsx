80cfa7b4713d63e91bb5bdbcfb98ac9cdda919f0
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-05 23:00:26 +0100

    Add search button to add courses page

--- src/components/courses/select/select-course-list-control.tsx
+++ src/components/courses/select/select-course-list-control.tsx
@@ -1,5 +1,13 @@
-import { Flex, Spacer, Text } from "@chakra-ui/react";
-import { IoList, IoGrid } from "react-icons/io5";
+import {
+  Flex,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { IoList, IoGrid, IoSearchOutline } from "react-icons/io5";
 import RadioButtonGroup from "../../common/radio-button-group";
 
 type SelectCourseListControlProps = {
@@ -18,31 +26,47 @@ export default function SelectCourseListControl({
   onViewChange,
 }: SelectCourseListControlProps) {
   return (
-    <Flex
-      direction={["column", null, "row"]}
-      rowGap={4}
-      align="center"
-      justify="space-between"
-    >
-      <RadioButtonGroup
-        display={["none", null, "flex"]}
-        options={[<IoList key="list" />, <IoGrid key="grid" />]}
-        labels={["List View", "Grid View"]}
-        values={["list", "grid"]}
-        value={view}
-        onChange={onViewChange}
-      />
-      <Spacer display={["none", null, "unset"]} />
-      <Text fontSize="lg" fontWeight="semibold">
-        {session} Session
-      </Text>
-      <Spacer />
-      <RadioButtonGroup
-        values={["all", "first", "second"]}
-        labels={["All", "1st Semester", "2nd Semester"]}
-        value={semester}
-        onChange={onSemesterChange}
-      />
-    </Flex>
+    <>
+      <Flex
+        direction={["column", null, "row"]}
+        rowGap={4}
+        align="center"
+        justify="space-between"
+      >
+        <RadioButtonGroup
+          display={["none", null, "flex"]}
+          options={[<IoList key="list" />, <IoGrid key="grid" />]}
+          labels={["List View", "Grid View"]}
+          values={["list", "grid"]}
+          value={view}
+          onChange={onViewChange}
+        />
+        <Spacer display={["none", null, "unset"]} />
+        <Text fontSize="lg" fontWeight="semibold">
+          {session} Session
+        </Text>
+        <Spacer />
+        <RadioButtonGroup
+          values={["all", "first", "second"]}
+          labels={["All", "1st Semester", "2nd Semester"]}
+          value={semester}
+          onChange={onSemesterChange}
+        />
+      </Flex>
+      <Flex mt={4} justify="flex-end">
+        <InputGroup pr={0} w={80} variant="primary">
+          <Input pr={4} type="search" />
+          <InputRightElement>
+            <IconButton
+              aria-label="Search for course"
+              icon={<IoSearchOutline fontSize="1.5rem" />}
+              h="1.75rem"
+              size="sm"
+              // onClick={}
+            />
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </>
   );
 }
