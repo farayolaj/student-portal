b70aa5a8fc4b28ea7995dbd453e72c90aa5f8af7
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-10 15:08:26 +0100

    Set up react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,18 +5,24 @@ import theme from "../theme";
 import "../components/common/calendar.css";
 import Layout, { LayoutProps } from "../components/layout";
 import { NextComponentType } from "next";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "../lib/query-client";
 
 type CustomAppProps = AppProps & {
-  Component: NextComponentType & { layoutProps: LayoutProps};
-}
+  Component: NextComponentType & { layoutProps: LayoutProps };
+};
 
 export default function App({ Component, pageProps }: CustomAppProps) {
   const layoutProps = Component.layoutProps || {};
   return (
     <ChakraProvider theme={theme}>
-      <Layout {...layoutProps}>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout {...layoutProps}>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
