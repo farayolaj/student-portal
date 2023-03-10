fce79d1e4824281673346be4729046178b00632e
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-10 17:44:20 +0100

    Added AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { NextComponentType } from "next";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import queryClient from "../lib/query-client";
+import { AuthProvider } from "../hooks/use-auth";
 
 type CustomAppProps = AppProps & {
   Component: NextComponentType & { layoutProps: LayoutProps };
@@ -18,9 +19,11 @@ export default function App({ Component, pageProps }: CustomAppProps) {
   return (
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <Layout {...layoutProps}>
-          <Component {...pageProps} />
-        </Layout>
+        <AuthProvider>
+          <Layout {...layoutProps}>
+            <Component {...pageProps} />
+          </Layout>
+        </AuthProvider>
         <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
     </ChakraProvider>
