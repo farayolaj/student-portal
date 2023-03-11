6f4014b47417cdba25f87a927725c7fddc8991a6
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-11 13:41:43 +0100

    Added new abstract images

--- src/lib/get-abstract-image.ts
+++ src/lib/get-abstract-image.ts
@@ -3,6 +3,11 @@ import abstractTwo from "../images/abstract_2.jpg";
 import abstractThree from "../images/abstract_3.jpg";
 import abstractFour from "../images/abstract_4.jpg";
 import abstractFive from "../images/abstract_5.jpg";
+import abstractSix from "../images/abstract_6.jpg";
+import abstractSeven from "../images/abstract_7.jpg";
+import abstractEight from "../images/abstract_8.jpg";
+import abstractNine from "../images/abstract_9.jpg";
+import abstractTen from "../images/abstract_10.jpg";
 
 let idx = 0;
 
@@ -12,6 +17,11 @@ const abstractImages = [
   abstractThree,
   abstractFour,
   abstractFive,
+  abstractSix,
+  abstractSeven,
+  abstractEight,
+  abstractNine,
+  abstractTen,
 ];
 
 const getAbstractImage = () => {
