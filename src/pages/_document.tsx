800a4fea89159ebfedf0aa92369e8d62fd36ff78
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-15 21:48:15 +0100

    Fixed initial base url fetching error
    The X-APP-KEY header was absent, but required. This fix adds it.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,8 @@ import Script from "next/script";
 
 export default function Document() {
   const initialBaseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const xAppKey = process.env.NEXT_PUBLIC_X_APP_KEY;
+
   return (
     <Html lang="en">
       <Head />
@@ -13,10 +15,21 @@ export default function Document() {
           {`
           const hostname = encodeURIComponent(window.location.hostname);
           const url = "${initialBaseUrl}/baseUrl?domain=" + hostname;
-          fetch(url, { headers: { "ngrok-skip-browser-warning": "," } })
-              .then((res) => res.json())
-              .then((res) => localStorage.setItem("apiBaseUrl", res.payload))
-              .catch(err => { if (!document.location.href.includes('/error')) document.location.href = "/error"; })`}
+          
+          fetch(url, {
+            headers: { "ngrok-skip-browser-warning": ",", "X-APP-KEY": "${xAppKey}" },
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              if (!res.status) throw new Error(res.message);
+              return res.payload;
+            })
+            .then((payload) => localStorage.setItem("apiBaseUrl", payload))
+            .catch((err) => {
+              if (!document.location.href.includes("/error"))
+                document.location.href = "/error";
+            });
+          `}
         </Script>
       </body>
     </Html>
