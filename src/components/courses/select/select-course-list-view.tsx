aa00a064a8dc47091fc5fcdf503aac5a8d7e18ad
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-16 13:49:27 +0100

    Fixed course selection checkbox

--- src/components/courses/select/select-course-list-view.tsx
+++ src/components/courses/select/select-course-list-view.tsx
@@ -72,9 +72,17 @@ const SelectCourseListViewItem: FC<SelectCourseListViewItemProps> = ({
       borderColor="transparent"
       _checked={{ borderColor: colorScheme500 }}
       _hover={{ "&[aria-checked=false]": { borderColor: colorScheme300 } }}
-      onClick={() => onToggleSelection(course.id)}
       cursor="pointer"
     >
+      <Box
+        pos="absolute"
+        zIndex={200}
+        w="full"
+        h="full"
+        top={0}
+        left={0}
+        onClick={() => onToggleSelection(course.id)}
+      />
       <Checkbox
         pos="absolute"
         top={4}
@@ -85,7 +93,6 @@ const SelectCourseListViewItem: FC<SelectCourseListViewItemProps> = ({
         value={course.id}
         isChecked={isChecked}
         borderColor={colorScheme500}
-        onClick={() => onToggleSelection(course.id)}
       />
       <Flex>
         <Box pos="relative" w="15rem" minH="15rem" overflow="hidden">
