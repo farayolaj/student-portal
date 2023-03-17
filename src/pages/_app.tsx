0fae543c4724459752eb655861ce37ed2af3456f
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-17 10:19:09 +0100

    Redirect to error page when getting api base url fails

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,18 +10,26 @@ import queryClient from "../lib/query-client";
 import { AuthProvider } from "../hooks/use-auth";
 import { useEffect } from "react";
 import getApi from "../api/api";
+import { useRouter } from "next/router";
+import { ERROR_PAGE } from "@/constants/routes";
 
 type CustomAppProps = AppProps & {
   Component: NextComponentType & { layoutProps: LayoutProps };
 };
 
 export default function App({ Component, pageProps }: CustomAppProps) {
+  const router = useRouter();
+
   const loadBasePath = async () => {
-    const initialBaseUrl = process.env.NEXT_PUBLIC_API_URL;
-    const hostname = encodeURIComponent(window.location.hostname);
-    const url = `${initialBaseUrl}/baseUrl?domain=${hostname}`;
-    const res = await getApi().get(url);
-    localStorage.setItem("apiBaseUrl", res.data.payload);
+    try {
+      const initialBaseUrl = process.env.NEXT_PUBLIC_API_URL;
+      const hostname = encodeURIComponent(window.location.hostname);
+      const url = `${initialBaseUrl}/baseUrl?domain=${hostname}`;
+      const res = await getApi().get(url);
+      localStorage.setItem("apiBaseUrl", res.data.payload);
+    } catch (error) {
+      router.push(ERROR_PAGE);
+    }
   };
 
   useEffect(() => {
