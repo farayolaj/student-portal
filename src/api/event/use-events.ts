5c517d2b544f438b3bcf816c27a693ec545d152f
Joshua Farayola <farayolajoshua@gmail.com>
2023-03-20 02:47:37 +0100

    Fully implement events api query

--- src/api/event/use-events.ts
+++ src/api/event/use-events.ts
@@ -1,16 +1,13 @@
-import { toEventDateMapping } from "@/transformers/events";
+import { toCalendarEvent, toEventDateMapping } from "@/transformers/events";
 import { createQuery } from "react-query-kit";
+import getApi from "../api";
 
 export const useEvents = createQuery("events", async () => {
-  // const response = await getApi().get("/event");
+  const response = await getApi().get("/event");
 
-  // if (!response.data.status) throw new Error(response.data.message);
+  if (!response.data.status) throw new Error(response.data.message);
 
-  // return toEventDateMapping(
-  //   (response.data.payload?.map(toCalendarEvent) as CalendarEvent[]) || []
-  // );
-
-  return toEventDateMapping([
-    { name: "Examination Starts", date: new Date(2023, 2, 20) },
-  ]);
+  return toEventDateMapping(
+    (response.data.payload?.map(toCalendarEvent) as CalendarEvent[]) || []
+  );
 });
