e5fa35a740de7297b725c80724dd6cf36c6787ba
Joshua Farayola <farayolajoshua@gmail.com>
2023-04-12 21:12:44 +0100

    Added payment detail api

---
+++ src/api/payment/use-payment-detail.ts
@@ -0,0 +1,20 @@
+import { toPayment } from "@/transformers/payments";
+import { createQuery } from "react-query-kit";
+import getApi from "../api";
+
+export const usePaymentDetail = createQuery<Payment, { id: string }>(
+  "payment-detail",
+  async ({ queryKey: [_, { id }] }) => {
+    const response = await getApi().get(
+      `/fee_details?pid=${encodeURIComponent(id)}`
+    );
+
+    if (!response.data.status) throw new Error(response.data.message);
+
+    const data = response.data.payload;
+    return toPayment({
+      ...data.payment_details,
+      transaction: data.split_payment,
+    });
+  }
+);
