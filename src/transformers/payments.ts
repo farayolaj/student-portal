1a444bb5bf5b103db2a58de42bf28e9874537a78
Joshua Farayola <farayolajoshua@gmail.com>
2023-04-12 21:15:53 +0100

    Updated payment transformer

--- src/transformers/payments.ts
+++ src/transformers/payments.ts
@@ -6,6 +6,22 @@ export function toPayment(data: any): Payment {
     amount: data.total,
     status: data.paid ? "paid" : "unpaid",
     title: data.description,
-    dueDate: parse(data.due_date, "MMM. dd, yyyy", new Date()),
+    dueDate: parse(data.date_due || data.due_date, "MMM. dd, yyyy", new Date()),
+    level: data.level ? data.level + "00" : undefined,
+    sessionId: data.session,
+    transaction: data.transaction
+      ? {
+          id: data.transaction.order_id,
+          amount: data.total,
+          // Todo: Get the correct initiation date
+          dateInitiated: new Date(),
+          description: data.description,
+          referenceNumber: data.transaction.order_id,
+          rrr: data.transaction.rrr,
+          status: data.paid ? "success" : "pending",
+          // Todo: Get the correct date paid
+          datePayed: new Date(),
+        }
+      : undefined,
   };
 }
