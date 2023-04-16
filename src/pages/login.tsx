9b8c769242cd14cfbfaa579ff9a20b4f582455b7
Joshua Farayola <farayolajoshua@gmail.com>
2023-04-16 16:00:43 +0100

    Redesigned login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -18,7 +18,7 @@ import {
 import Image from "next/image";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
-import { useState, FormEvent } from "react";
+import { useEffect, useState, FormEvent } from "react";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { useValidateUsername } from "../api/auth/use-validate-username";
 import Seo from "../components/common/seo";
@@ -26,7 +26,10 @@ import * as routes from "../constants/routes";
 import useAuth from "../hooks/use-auth";
 
 import uiLogo from "../images/ui-logo.png";
-import studentImage from "../images/student_image.jpg";
+import blueGreenDiamonds from "../images/blue_green_diamonds.png";
+import greenery from "../images/greenery.png";
+
+const bgImages = [blueGreenDiamonds, greenery];
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -38,6 +41,15 @@ export default function Login() {
   const router = useRouter();
   const toast = useToast();
   const validateUsername = useValidateUsername();
+
+  const [bgImageIdx, setBgImageIdx] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBgImageIdx((bgImageIdx + 1) % bgImages.length);
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [bgImageIdx]);
 
   const onValidateUsername = () => {
     validateUsername.mutate(
@@ -88,125 +100,159 @@ export default function Login() {
   return (
     <>
       <Seo title="Login" />
-      <Flex wrap={["wrap", null, "nowrap"]} minH="100vh">
-        <Box w={["full", null, "33.4%"]} order={[1, null, 2]}>
-          <Flex align="center" py={8} direction="column" gap={2}>
-            <Image src={uiLogo} alt="University of Ibadan Logo" width={64} />
-            <Box>
-              <Text
-                as="span"
-                display="block"
-                size="md"
-                textAlign="center"
-                textTransform="uppercase"
-                fontWeight="bold"
-              >
-                Distance Learning Center
-              </Text>
-              <Text
-                as="span"
-                display="block"
-                textAlign="center"
-                textTransform="uppercase"
-                fontWeight="semibold"
-                fontSize="sm"
-              >
-                University of Ibadan
-              </Text>
-            </Box>
-          </Flex>
-          <Heading textAlign="center" size="md" mt={8}>
-            Log In to Student Portal
-          </Heading>
-          <chakra.form
-            mt={16}
-            display="flex"
-            flexDir="column"
-            gap={6}
-            onSubmit={onSubmit}
-            w="80%"
-            mx="auto"
-          >
-            <FormControl isReadOnly={usernameVerified} isRequired>
-              <FormLabel fontSize="sm" fontWeight="bold">
-                Matric. Number/Email Address
-              </FormLabel>
-              <Input
-                name="username"
-                placeholder="Enter matric. number or email address"
-                size="sm"
-                onChange={(e) => setUsername(e.target.value)}
-                autoComplete="username"
-                hidden={usernameVerified}
-              />
-              {usernameVerified && (
-                <Text fontSize="sm" py={1} px={3}>
-                  {username}
-                </Text>
-              )}
-            </FormControl>
-            {usernameVerified && (
-              <FormControl isRequired>
-                <FormLabel fontSize="sm" fontWeight="bold">
-                  Password
-                </FormLabel>
-                <InputGroup size="sm">
-                  <Input
-                    name="password"
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Default password is your last name in lowercase"
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
-                  />
-                  <InputRightElement>
-                    <IconButton
-                      variant="unstyled"
-                      aria-label="Toggle password visibility"
-                      boxSize={6}
-                      onClick={togglePasswordVisibility}
-                      icon={
-                        showPassword ? <IoEyeOutline /> : <IoEyeOffOutline />
-                      }
-                    />
-                  </InputRightElement>
-                </InputGroup>
-                <FormHelperText>
-                  <Link as={NextLink} href={routes.FORGOT_PASSWORD}>
-                    Forgot password?
-                  </Link>
-                </FormHelperText>
-              </FormControl>
-            )}
-            <Box mt={16} mb={8}>
-              {usernameVerified ? (
-                <Button w="full" type="submit" isDisabled={auth.isLoggingIn}>
-                  Log In
-                </Button>
-              ) : (
-                <Button
-                  w="full"
-                  isDisabled={validateUsername.isLoading}
-                  type="submit"
-                >
-                  Next
-                </Button>
-              )}
-            </Box>
-          </chakra.form>
-        </Box>
-        <Box
-          bg="gray.200"
-          w={["full", null, "66.6%"]}
-          order={[2, null, 1]}
-          pos="relative"
+      <Flex
+        position="sticky"
+        align="center"
+        gap={4}
+        top="0px"
+        zIndex={999}
+        as="header"
+        bg="white"
+        py={2}
+        px={8}
+        h="4rem"
+        w="full"
+      >
+        <Image height={48} src={uiLogo} alt="University of Ibadan Logo" />
+        <Text
+          display={["none", null, "initial"]}
+          as="span"
+          fontSize="lg"
+          fontWeight="semibold"
         >
-          <Image
-            src={studentImage}
-            alt="Students in a class"
-            fill
-            style={{ objectFit: "cover" }}
-          />
+          University of Ibadan
+        </Text>
+      </Flex>
+      <Flex wrap={["wrap", null, "nowrap"]} minH="calc(100vh - 4rem)">
+        <Box
+          bg="primary.500"
+          w={["full", null, "66.6%"]}
+          p={[null, null, "5rem"]}
+        >
+          <Box
+            pos="relative"
+            h="full"
+            _before={{
+              content: "''",
+              display: ["none", null, "block"],
+              pos: "absolute",
+              w: "100%",
+              h: "100%",
+              top: "1.5rem",
+              left: "1.5rem",
+              bg: "white",
+            }}
+          >
+            <Image
+              src={bgImages[bgImageIdx]}
+              alt=""
+              fill
+              style={{ objectFit: "cover" }}
+            />
+            <Box
+              w={[null, null, "50%"]}
+              pos="absolute"
+              p="2rem"
+              bg="primary.500"
+              bottom={[null, null, "-1.5rem"]}
+              color="white"
+            >
+              <Heading as="h2" size="lg">
+                Student Portal
+              </Heading>
+              <Text mt={2}>
+                Get to know your new community! This special section for
+                admitted students includes a welcome message, posts from your
+                new classmates, and important information about preparing to
+                enroll.
+              </Text>
+            </Box>
+          </Box>
         </Box>
+        <Flex align="center" w={["full", null, "33.4%"]} bg="primary.50">
+          <Box w="full">
+            <Heading textAlign="center" size="md" mt={8}>
+              Log In
+            </Heading>
+            <chakra.form
+              mt={16}
+              display="flex"
+              flexDir="column"
+              gap={6}
+              onSubmit={onSubmit}
+              w="80%"
+              mx="auto"
+            >
+              <FormControl isReadOnly={usernameVerified} isRequired>
+                <FormLabel fontSize="sm" fontWeight="bold">
+                  Matric. Number/Email Address
+                </FormLabel>
+                <Input
+                  bg="white"
+                  name="username"
+                  placeholder="Enter matric. number or email address"
+                  size="sm"
+                  onChange={(e) => setUsername(e.target.value)}
+                  autoComplete="username"
+                  hidden={usernameVerified}
+                />
+                {usernameVerified && (
+                  <Text fontSize="sm" py={1} px={3}>
+                    {username}
+                  </Text>
+                )}
+              </FormControl>
+              {usernameVerified && (
+                <FormControl isRequired>
+                  <FormLabel fontSize="sm" fontWeight="bold">
+                    Password
+                  </FormLabel>
+                  <InputGroup size="sm">
+                    <Input
+                      bg="white"
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Default password is your last name in lowercase"
+                      onChange={(e) => setPassword(e.target.value)}
+                      autoComplete="current-password"
+                    />
+                    <InputRightElement>
+                      <IconButton
+                        variant="unstyled"
+                        aria-label="Toggle password visibility"
+                        boxSize={6}
+                        onClick={togglePasswordVisibility}
+                        icon={
+                          showPassword ? <IoEyeOutline /> : <IoEyeOffOutline />
+                        }
+                      />
+                    </InputRightElement>
+                  </InputGroup>
+                  <FormHelperText>
+                    <Link as={NextLink} href={routes.FORGOT_PASSWORD}>
+                      Forgot password?
+                    </Link>
+                  </FormHelperText>
+                </FormControl>
+              )}
+              <Box mt={16} mb={8}>
+                {usernameVerified ? (
+                  <Button w="full" type="submit" isDisabled={auth.isLoggingIn}>
+                    Log In
+                  </Button>
+                ) : (
+                  <Button
+                    w="full"
+                    isDisabled={validateUsername.isLoading}
+                    type="submit"
+                  >
+                    Next
+                  </Button>
+                )}
+              </Box>
+            </chakra.form>
+          </Box>
+        </Flex>
       </Flex>
     </>
   );
