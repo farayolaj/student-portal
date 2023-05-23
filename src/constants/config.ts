a907da8c73e74bcad9746301a6ab79820155820e
Joshua Farayola <farayolajoshua@gmail.com>
2023-05-23 13:15:12 +0100

    Implemented auth wall

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 export const X_APP_KEY = process.env.NEXT_PUBLIC_X_APP_KEY || "";
+export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
+export const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "";
