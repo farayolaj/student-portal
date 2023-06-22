99b2fe1ac6f7f8787480dd973c79e055caa800cd
Joshua Farayola <farayolajoshua@gmail.com>
2023-06-22 08:07:03 +0100

    Improved mails loading

--- src/pages/my-mails.tsx
+++ src/pages/my-mails.tsx
@@ -41,7 +41,23 @@ export default function MessageLayout() {
                 allMessagesRes.data?.pages.reduce(
                   (prev, curr) => [...prev, ...curr],
                   []
-                ) || []
+                ) || [
+                  "1",
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10",
+                  "11",
+                  "12",
+                  "13",
+                  "14",
+                  "15",
+                ]
               }
               onSelect={setSelectedMessageId}
               selectedId={selectedMessageId}
