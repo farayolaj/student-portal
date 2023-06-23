01fa498619a603c722d3d7e74383c7bf01961bc5
Joshua Farayola <farayolajoshua@gmail.com>
2023-06-23 08:03:46 +0100

    Increase event data stale time

--- src/api/event/use-events.ts
+++ src/api/event/use-events.ts
@@ -2,12 +2,18 @@ import { toCalendarEvent, toEventDateMapping } from "@/transformers/events";
 import { createQuery } from "react-query-kit";
 import getApi from "../api";
 
-export const useEvents = createQuery("events", async () => {
-  const response = await getApi().get("/student_events");
+export const useEvents = createQuery(
+  "events",
+  async () => {
+    const response = await getApi().get("/student_events");
 
-  if (!response.data.status) throw new Error(response.data.message);
+    if (!response.data.status) throw new Error(response.data.message);
 
-  return toEventDateMapping(
-    (response.data.payload?.map(toCalendarEvent) as CalendarEvent[]) || []
-  );
-});
+    return toEventDateMapping(
+      (response.data.payload?.map(toCalendarEvent) as CalendarEvent[]) || []
+    );
+  },
+  {
+    staleTime: 1000 * 60 * 60 * 24,
+  }
+);
