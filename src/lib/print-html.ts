190a2b12d9886a22da559e74d68fb8e35bd51750
Joshua Farayola <farayolajoshua@gmail.com>
2023-07-21 10:15:19 +0100

    Update printHtml function to trigger print after a delay

--- src/lib/print-html.ts
+++ src/lib/print-html.ts
@@ -1,6 +1,7 @@
 export default async function printHtml(html: string, filename: string) {
   const win = window.open("", filename) as Window;
   win.document.write(html);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   win.print();
-  win.close();
+  // win.close();
 }
