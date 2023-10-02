a7b04cf61a2c21c97bedd3e586f230b404f5a2d5
Joshua Farayola <farayolajoshua@gmail.com>
2023-10-02 09:59:26 +0100

    Add link to dashboard home in error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,20 +1,38 @@
-import { Card, CardBody, CardHeader, Flex, Heading } from "@chakra-ui/react";
+import { HOME } from "@/constants/routes";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Link,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
 
 export default function Error() {
   return (
     <Flex w="full" minH="100vh" align="center" justify="center" bg="gray.200">
-      <Card w="40%" py={6} px={12}>
+      <Card w="40%" py={6} px={12} textAlign={"center"}>
         <CardHeader>
           <Heading size="md">Oops! We&apos;ve done something wrong!</Heading>
         </CardHeader>
         <CardBody>
           Don&apos;t worry, we&apos;re working on fixing this already!
+          <Box mt={6}>
+            <Link
+              textAlign="center"
+              as={NextLink}
+              href={HOME}
+              variant={"button"}
+            >
+              Go Back Home
+            </Link>
+          </Box>
         </CardBody>
       </Card>
     </Flex>
   );
-
-  return <div>Oops! We&apos;ve done something wrong! We&apos;re fixing it</div>;
 }
 
 Error.layoutProps = {
