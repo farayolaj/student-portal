588c32efb4b03728540ef8030cae8339d7f64b60
holynation <holynation667@gmail.com>
2024-05-01 20:41:34 +0100

    fix: Added practicum to the dashboard

--- src/components/dashboard/practicum-form-card.tsx
+++ src/components/dashboard/practicum-form-card.tsx
@@ -1,5 +1,6 @@
 import { usePracticumEligibility } from "@/api/dashboard/use-practicum-form-eligibility";
 import { usePracticumRequestForm } from "@/api/dashboard/use-praticum-form-submit";
+import { useDownloadDocument } from "@/api/document/use-download-document";
 import {
   Card,
   CardBody,
@@ -12,6 +13,11 @@ import {
   Button,
   useToast,
   Textarea,
+  Text,
+  Badge,
+  Box,
+  HStack,
+  Link,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -44,7 +50,17 @@ const PraticumFormCard = () => {
   const { mutate: submitForm } = usePracticumRequestForm();
 
   const { data: eligibility } = usePracticumEligibility();
-  console.log(eligibility);
+  const { intiateFetch, isLoading } = useDownloadDocument({
+    url: eligibility?.payload?.print_url,
+    onError: (error) => {
+      toast({
+        title: error.message,
+        description: `Practicum form could not be downloaded. Please try again later.`,
+        status: "error",
+        isClosable: true,
+      });
+    },
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -82,105 +98,162 @@ const PraticumFormCard = () => {
     }));
   };
 
-  return (
-    <Card
-      height={"max-content"}
-      background={"white"}
-      display={
-        eligibility?.status && !eligibility?.payload?.is_submitted
-          ? "flex"
-          : "none"
-      }
-      p={"1rem"}
-      mt={"2rem"}
-    >
-      <CardHeader>
-        <Heading as="h2" fontSize="md">
-          {`${eligibility?.payload.course_code}`} Practicum Form
-        </Heading>
-      </CardHeader>
-      <CardBody>
-        <chakra.form
-          justifyContent={"space-between"}
-          onSubmit={handleSubmit}
-          alignItems={"center"}
-          flexWrap={"wrap"}
-          display={"flex"}
-          rowGap={"2rem"}
+  const DisplaySupervisor = ({ practicumForm } : any) => {
+    return (
+      <>
+        <Card
+        height={"max-content"}
+        background={"white"}
+        display="flex"
+        p={"1rem"}
+        mt={"2rem"}
         >
-          <FormControl isRequired w={"47%"}>
-            <FormLabel>Proposed School Name</FormLabel>
-            <Input
-              type="text"
-              name="schoolName"
-              value={formState.schoolName}
-              onChange={handleInputChange}
-            />
-          </FormControl>
-          <FormControl isRequired w={"47%"}>
-            <FormLabel>Proposed School Contact Address</FormLabel>
-            <Textarea
-              name="schoolContact"
-              value={formState.schoolContact}
-              onChange={handleInputChange}
-            />
-          </FormControl>
+          <CardHeader>
+            <Heading as="h2" fontSize="md">
+              {`${practicumForm?.course_code}`} Practicum Form
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Box mb="1">
+              <HStack>
+                <Text fontSize="md" fontWeight="600">Approved Practicum School:</Text>
+                <span>{practicumForm?.approved_practicum_school}</span> 
+              </HStack>
+            </Box>
+            <Box>
+              <HStack>
+                <Text fontSize="md" fontWeight="600">Approved Supervisor:</Text>
+                <span>{practicumForm?.approved_supervisor}</span> 
+              </HStack>
+            </Box>
 
-          <FormControl isRequired w={"45%"}>
-            <FormLabel>Proposed School City</FormLabel>
-            <Input
-              type="text"
-              name="schoolCity"
-              value={formState.schoolCity}
-              onChange={handleInputChange}
-            />
-          </FormControl>
+            {
+              practicumForm?.print_url && practicumForm?.print_url.length > 0 && (
+                <Box>
+                  <Text>
+                    Kindly click the link to download your document. { }
+                    <Badge colorScheme="green" p="2" borderRadius="md">
+                      <Link href="#" onClick={() => intiateFetch()}> Click here</Link>
+                    </Badge>
+                  </Text>
+                </Box>
+              )
+            }
+            
+          </CardBody>
+        </Card>
+      </>
+    )
+  }
 
-          <FormControl isRequired w={"47%"}>
-            <FormLabel>Proposed School Local Government</FormLabel>
-            <Input
-              type="text"
-              name="schoolLGA"
-              value={formState.schoolLGA}
-              onChange={handleInputChange}
-            />
-          </FormControl>
+  return (
+    <>
+      {
+        eligibility?.status && eligibility?.payload?.is_submitted && (
+          <DisplaySupervisor practicumForm={eligibility?.payload} />
+        )
+      }
+      
+      <Card
+        height={"max-content"}
+        background={"white"}
+        display={
+          eligibility?.status && !eligibility?.payload?.is_submitted
+            ? "flex"
+            : "none"
+        }
+        p={"1rem"}
+        mt={"2rem"}
+      >
+        <CardHeader>
+          <Heading as="h2" fontSize="md">
+            {`${eligibility?.payload.course_code}`} Practicum Form
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <chakra.form
+            justifyContent={"space-between"}
+            onSubmit={handleSubmit}
+            alignItems={"center"}
+            flexWrap={"wrap"}
+            display={"flex"}
+            rowGap={"2rem"}
+          >
+            <FormControl isRequired w={"47%"}>
+              <FormLabel>Proposed School Name</FormLabel>
+              <Input
+                type="text"
+                name="schoolName"
+                value={formState.schoolName}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+            <FormControl isRequired w={"47%"}>
+              <FormLabel>Proposed School Contact Address</FormLabel>
+              <Textarea
+                name="schoolContact"
+                value={formState.schoolContact}
+                onChange={handleInputChange}
+              />
+            </FormControl>
 
-          <FormControl isRequired w={"47%"}>
-            <FormLabel>Proposed School State</FormLabel>
-            <Input
-              type="text"
-              name="schoolState"
-              value={formState.schoolState}
-              onChange={handleInputChange}
-            />
-          </FormControl>
+            <FormControl isRequired w={"45%"}>
+              <FormLabel>Proposed School City</FormLabel>
+              <Input
+                type="text"
+                name="schoolCity"
+                value={formState.schoolCity}
+                onChange={handleInputChange}
+              />
+            </FormControl>
 
-          <FormControl isRequired w={"47%"}>
-            <FormLabel>Proposed School Location Description</FormLabel>
-            <Textarea
-              name="schoolLocationDesc"
-              value={formState.schoolLocationDesc}
-              onChange={handleInputChange}
-            />
-          </FormControl>
+            <FormControl isRequired w={"47%"}>
+              <FormLabel>Proposed School Local Government</FormLabel>
+              <Input
+                type="text"
+                name="schoolLGA"
+                value={formState.schoolLGA}
+                onChange={handleInputChange}
+              />
+            </FormControl>
 
-          <FormControl isRequired w={"47%"}>
-            <FormLabel>Student Active Telephone Number</FormLabel>
-            <Input
-              type="number"
-              name="schoolPhone"
-              value={formState.schoolPhone}
-              onChange={handleInputChange}
-            />
-          </FormControl>
+            <FormControl isRequired w={"47%"}>
+              <FormLabel>Proposed School State</FormLabel>
+              <Input
+                type="text"
+                name="schoolState"
+                value={formState.schoolState}
+                onChange={handleInputChange}
+              />
+            </FormControl>
 
-          <Button type="submit" alignSelf={"flex-end"}>
-            Submit Form
-          </Button>
-        </chakra.form>
-      </CardBody>
-    </Card>
+            <FormControl isRequired w={"47%"}>
+              <FormLabel>Proposed School Location Description</FormLabel>
+              <Textarea
+                name="schoolLocationDesc"
+                value={formState.schoolLocationDesc}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+
+            <FormControl isRequired w={"47%"}>
+              <FormLabel>Student Active Telephone Number</FormLabel>
+              <Input
+                type="number"
+                name="schoolPhone"
+                value={formState.schoolPhone}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+
+            <Button type="submit" alignSelf={"flex-end"}>
+              Submit Form
+            </Button>
+          </chakra.form>
+        </CardBody>
+      </Card>
+    </>
+    
   );
 };
 
