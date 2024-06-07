368f3a153c1a9a355c766ed47c8eb16d3350a860
Joshua Farayola <farayolajoshua@gmail.com>
2024-06-07 07:19:01 +0100

    hide portal alert header or body when empty

--- src/api/common/use-portal-alert.ts
+++ src/api/common/use-portal-alert.ts
@@ -9,8 +9,16 @@ export const usePortalAlert = createQuery<{ header: string; body: string }>(
     if (!response.data.status) throw new Error(response.data.message);
 
     return {
-      header: response.data.payload.notice_header,
-      body: response.data.payload.notice_body,
+      header:
+        response.data.payload.notice_header &&
+        response.data.payload.notice_header.length > 0
+          ? response.data.payload.notice_header
+          : undefined,
+      body:
+        response.data.payload.notice_body &&
+        response.data.payload.notice_body.length > 0
+          ? response.data.payload.notice_body
+          : undefined,
     };
   }
 );
