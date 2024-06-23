f97d282362138d012914ffa967abf8e72b524d98
Alatise Oluwaseun <holynation667@gmail.com>
2024-06-23 23:48:29 +0100

    feat: Added new payment description to the school board

--- src/components/layout/schoolboard-sidebar.tsx
+++ src/components/layout/schoolboard-sidebar.tsx
@@ -17,7 +17,7 @@ import { FC } from "react";
 import EventCalendar from "../common/events/event-calendar";
 import useRemitaInline from "../common/remita-inline";
 
-const mostSubscribedSundryCodes = ["76", "6"];
+const mostSubscribedSundryCodes = ["76", "57"];
 
 const DisplayPanel: FC = () => {
   const sundryPaymentsQuery = useSundryPayments({
