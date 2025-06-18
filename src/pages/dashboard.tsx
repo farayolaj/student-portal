f1f667b6c2923f792fc102bca9e90e21565b7c9d
Joshua Farayola <farayolajoshua@gmail.com>
2025-06-18 18:31:08 +0100

    fix: remove commented-out profile survey code
    Clean up the dashboard component by removing unused
    commented-out code related to the profile survey.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,26 +7,12 @@ import PaymentsCard from "../components/dashboard/payments-card";
 import ProfileCard from "../components/dashboard/profile-card";
 
 export default function Home() {
-  // const profile = useProfile();
   return (
     <>
       <Seo title="Dashboard" />
       <PageTitle>Dashboard</PageTitle>
-      {/* {profile.data && (
-        <Box mb={8}>
-          <LMSOrientationSurvey
-            isFresher={profile.data.user.isFresher}
-            orientationAttendance={profile.data.user.orientationAttendance}
-            orientationAttendanceDate={
-              profile.data.user.orientationAttendanceDate
-            }
-            orientationSeatNo={profile.data.user.orientationSeatNo}
-          />
-        </Box>
-      )} */}
       <ProfileCard />
       <InitiateResultVerificationCard />
-      {/* <UpdateBioData /> */}
       <PraticumFormCard />
       <CoursesCard />
       <PaymentsCard />
