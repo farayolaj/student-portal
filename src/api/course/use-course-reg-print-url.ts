c42846acb56efc268376178fcb3e7426b041d972
Joshua Farayola <farayolajoshua@gmail.com>
2025-07-17 14:20:27 +0100

    feat: add prerequisites handling in course print URL
    Enhance the course registration print functionality by
    including prerequisites checks and displaying payment
    requirements in a popover.

--- src/api/course/use-course-reg-print-url.ts
+++ src/api/course/use-course-reg-print-url.ts
@@ -1,16 +1,34 @@
+import { TRANSACTION_TYPES } from "@/transformers/payments";
 import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import getApi from "../api";
 
+// Todo: Needs refactoring
 export function useCourseRegPrintUrl(sessionId: string, semester: number) {
   const semesterString = semester == 1 ? "first" : "second";
   const [url, setUrl] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  const [prerequisites, setPrerequisites] = useState<
+    Array<{
+      id: string;
+      name: string;
+      transactionType: Payment["transactionType"];
+      transactionRef?: string;
+    }>
+  >();
 
   useEffect(() => {
     setIsLoading(true);
     setError(null);
+    setPrerequisites(undefined);
+
+    if (!sessionId || !semesterString) {
+      setIsLoading(false);
+      setError(new Error("Session ID and semester are required"));
+      return;
+    }
+
     getApi()
       .get(
         `/courseregistrationprint?session=${encodeURIComponent(
@@ -24,6 +42,19 @@ export function useCourseRegPrintUrl(sessionId: string, semester: number) {
         }
       )
       .then((response) => {
+        if (response.headers["content-type"] === "application/json") {
+          // Only reason for a json response is an error
+          setIsLoading(false);
+          const data = response.data;
+
+          setError(
+            new Error(
+              data.message || "Error generating course registration print"
+            )
+          );
+          return;
+        }
+
         const url = URL.createObjectURL(
           new Blob([response.data], { type: "application/pdf" })
         );
@@ -33,7 +64,32 @@ export function useCourseRegPrintUrl(sessionId: string, semester: number) {
       .catch((error) => {
         setIsLoading(false);
         if (error instanceof AxiosError && error.status == 403) {
-          setError(new Error(error.response?.statusText));
+          const data = error.response?.data as Blob;
+
+          data.text().then((text) => {
+            const data = JSON.parse(text);
+
+            if (
+              data.payload &&
+              Array.isArray(data.payload) &&
+              data.payload.length > 0
+            ) {
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              const payload = data.payload as Array<any>;
+
+              setPrerequisites(
+                payload.map((p) => ({
+                  id: p.prerequisite,
+                  name: p.description,
+                  amount: p.amount,
+                  transactionType: TRANSACTION_TYPES[p.transactionType],
+                  transactionRef: p.transactionRef,
+                }))
+              );
+            } else {
+              setPrerequisites(undefined);
+            }
+          });
         } else {
           setError(
             new Error("Error generating course registration print", {
@@ -44,5 +100,5 @@ export function useCourseRegPrintUrl(sessionId: string, semester: number) {
       });
   }, [semesterString, sessionId]);
 
-  return { url, isLoading, error };
+  return { url, isLoading, error, prerequisites };
 }
--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,12 +1,17 @@
 import { useCourseRegPrintUrl } from "@/api/course/use-course-reg-print-url";
 import { useCurrentPeriod } from "@/api/user/use-current-period";
+import buildPaymentDetailUrl from "@/lib/payments/build-payment-detail-url";
 import {
   Button,
   Flex,
   Icon,
   Link,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
   Spinner,
-  Tooltip,
   useToast,
 } from "@chakra-ui/react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -170,25 +175,73 @@ const Courses: FC = () => {
             </Button>
           )}
           {!inDeleteCourseView && (
-            <Tooltip label={printUrl.error?.message ?? ""}>
-              <Button
-                mx="auto"
-                display="inline-flex"
-                gap={4}
-                isDisabled={printUrl.isLoading || !!printUrl.error}
-                onClick={() => {
-                  window.open(printUrl.url as string);
-                }}
-                title={printUrl.error?.message}
-              >
-                {printUrl.isLoading ? (
-                  <Spinner size="xs" color="white" />
-                ) : (
-                  <Icon as={IoPrintOutline} boxSize={6} />
-                )}
-                Print Course Registration
-              </Button>
-            </Tooltip>
+            <Popover trigger="hover">
+              <PopoverTrigger>
+                <Button
+                  mx="auto"
+                  display="inline-flex"
+                  gap={4}
+                  // isDisabled={
+                  //   printUrl.isLoading ||
+                  //   !!printUrl.error ||
+                  //   (!!printUrl.prerequisites &&
+                  //     printUrl.prerequisites.length > 0)
+                  // }
+                  onClick={() => {
+                    if (!printUrl.isLoading && printUrl.url)
+                      window.open(printUrl.url as string);
+                  }}
+                >
+                  {printUrl.isLoading ? (
+                    <Spinner size="xs" color="white" />
+                  ) : (
+                    <Icon as={IoPrintOutline} boxSize={6} />
+                  )}
+                  Print Course Registration
+                </Button>
+              </PopoverTrigger>
+              {(printUrl.prerequisites || printUrl.error?.message) && (
+                <PopoverContent bg={"gray.400"} color="white">
+                  <PopoverArrow bg={"gray.400"} />
+                  <PopoverBody rounded={"md"}>
+                    {printUrl.prerequisites ? (
+                      <p>
+                        You must pay all outstanding fees to print your course
+                        registration:{" "}
+                        {printUrl.prerequisites
+                          .map((item) => (
+                            <Link
+                              key={`${item.id}-${item.transactionRef}`}
+                              as={NextLink}
+                              href={buildPaymentDetailUrl({
+                                id: item.id,
+                                trxRef: item.transactionRef,
+                                trxType: item.transactionType,
+                              })}
+                              textDecorationStyle="solid"
+                              textDecorationLine="underline"
+                            >
+                              {item.name}
+                            </Link>
+                          ))
+                          .reduce((prev, curr, idx) => {
+                            if (
+                              idx !== 0 &&
+                              idx === (printUrl.prerequisites?.length || 0) - 1
+                            )
+                              prev.push(" and ");
+                            else if (idx !== 0) prev.push(", ");
+                            prev.push(curr);
+                            return prev;
+                          }, [] as (JSX.Element | string)[])}
+                      </p>
+                    ) : (
+                      printUrl.error?.message ?? ""
+                    )}
+                  </PopoverBody>
+                </PopoverContent>
+              )}
+            </Popover>
           )}
         </Flex>
         <RadioButtonGroup
