de7018afc6f73fd3e4634852cfb502a92b1799cc
Joshua Farayola <farayolajoshua@gmail.com>
2025-09-01 21:56:16 +0100

    refactor: simplify course management logic
    Removed unused delete course functionality and related state management to streamline the course registration process.

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -14,17 +14,10 @@ import {
   Spinner,
   useToast,
 } from "@chakra-ui/react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import NextLink from "next/link";
 import { FC, useEffect, useState } from "react";
-import {
-  IoAdd,
-  IoGrid,
-  IoList,
-  IoPrintOutline,
-  IoTrashOutline,
-} from "react-icons/io5";
-import { deleteCourses } from "../../api/course.mutations";
+import { IoAdd, IoGrid, IoList, IoPrintOutline } from "react-icons/io5";
 import { courseQueries } from "../../api/course.queries";
 import PageTitle from "../../components/common/page-title";
 import RadioButtonGroup from "../../components/common/radio-button-group";
@@ -32,7 +25,6 @@ import Seo from "../../components/common/seo";
 import CourseListControls from "../../components/courses/course-list-controls";
 import CourseOverview from "../../components/courses/course-overview";
 import CourseView from "../../components/courses/course-view";
-import DeleteCourseView from "../../components/courses/delete/delete-course-view";
 import * as routes from "../../constants/routes";
 
 const Courses: FC = () => {
@@ -42,13 +34,8 @@ const Courses: FC = () => {
   const currentSemester = period.semester.id;
   const [sessionId, setSessionId] = useState(currentSessionId);
   const [semester, setSemester] = useState(currentSemester);
-  const [inDeleteCourseView, setInDeleteCourseView] = useState(false);
-  const queryClient = useQueryClient();
   const { data: canRegisterCurrentSemester } = useQuery(
     courseQueries.registrationOpenBy(semester)
-  );
-  const { data: canDeleteCurrentSemester } = useQuery(
-    courseQueries.deletionOpenBy(semester)
   );
   const [view, setView] = useState("list");
   const {
@@ -83,50 +70,8 @@ const Courses: FC = () => {
   const canAddCourses =
     sessionId === currentSessionId &&
     semester === currentSemester &&
-    !inDeleteCourseView &&
     canRegisterCurrentSemester &&
     !registeredCoursesError;
-  const canDeleteCourses =
-    sessionId === currentSessionId &&
-    canRegisterCurrentSemester &&
-    !registeredCoursesError &&
-    canDeleteCurrentSemester;
-
-  const deleteCoursesMutation = useMutation({
-    mutationFn: deleteCourses,
-    onSuccess: () => {
-      queryClient.invalidateQueries(
-        courseQueries.registeredBy(sessionId, semester)
-      );
-      queryClient.invalidateQueries(
-        courseQueries.statisticsFor(sessionId, semester)
-      );
-    },
-  });
-  const onDelete = (ids: string[]) => {
-    deleteCoursesMutation.mutate(
-      { ids },
-      {
-        onSuccess: () => {
-          setInDeleteCourseView(false);
-          toast({
-            title: "Courses deleted successfully",
-            status: "success",
-            isClosable: true,
-          });
-        },
-        onError: (err) => {
-          const error = err as Error;
-          toast({
-            title: "Error deleting courses",
-            description: error.message,
-            status: "error",
-            isClosable: true,
-          });
-        },
-      }
-    );
-  };
 
   const printUrl = useCourseRegPrintUrl(sessionId, semester);
 
@@ -139,7 +84,7 @@ const Courses: FC = () => {
         onSessionIdChange={setSessionId}
         semester={semester}
         onSemesterChange={setSemester}
-        inDeleteView={inDeleteCourseView}
+        inDeleteView={false}
       />
       <CourseOverview
         maxUnits={courseStats?.maxUnits || 0}
@@ -164,85 +109,73 @@ const Courses: FC = () => {
               Add Courses
             </Link>
           )}
-          {canDeleteCourses && (
-            <Button
-              onClick={() => setInDeleteCourseView((prev) => !prev)}
-              display="inline-flex"
-              gap={4}
-            >
-              <Icon as={IoTrashOutline} boxSize={6} />
-              {inDeleteCourseView ? "Cancel" : "Delete Courses"}
-            </Button>
-          )}
-          {!inDeleteCourseView && (
-            <Popover trigger="hover">
-              <PopoverTrigger>
-                <Button
-                  mx="auto"
-                  display="inline-flex"
-                  gap={4}
-                  // isDisabled={
-                  //   printUrl.isLoading ||
-                  //   !!printUrl.error ||
-                  //   (!!printUrl.prerequisites &&
-                  //     printUrl.prerequisites.length > 0)
-                  // }
-                  onClick={() => {
-                    if (!printUrl.isLoading && printUrl.url)
-                      window.open(printUrl.url as string);
-                  }}
-                >
-                  {printUrl.isLoading ? (
-                    <Spinner size="xs" color="white" />
+          <Popover trigger="hover">
+            <PopoverTrigger>
+              <Button
+                mx="auto"
+                display="inline-flex"
+                gap={4}
+                // isDisabled={
+                //   printUrl.isLoading ||
+                //   !!printUrl.error ||
+                //   (!!printUrl.prerequisites &&
+                //     printUrl.prerequisites.length > 0)
+                // }
+                onClick={() => {
+                  if (!printUrl.isLoading && printUrl.url)
+                    window.open(printUrl.url as string);
+                }}
+              >
+                {printUrl.isLoading ? (
+                  <Spinner size="xs" color="white" />
+                ) : (
+                  <Icon as={IoPrintOutline} boxSize={6} />
+                )}
+                Print Course Registration
+              </Button>
+            </PopoverTrigger>
+            {(printUrl.prerequisites || printUrl.error?.message) && (
+              <PopoverContent bg={"gray.400"} color="white">
+                <PopoverArrow bg={"gray.400"} />
+                <PopoverBody rounded={"md"}>
+                  {printUrl.prerequisites ? (
+                    <p>
+                      You must pay all outstanding fees to print your course
+                      registration:{" "}
+                      {printUrl.prerequisites
+                        .map((item) => (
+                          <Link
+                            key={`${item.id}-${item.transactionRef}`}
+                            as={NextLink}
+                            href={buildPaymentDetailUrl({
+                              id: item.id,
+                              trxRef: item.transactionRef,
+                              trxType: item.transactionType,
+                            })}
+                            textDecorationStyle="solid"
+                            textDecorationLine="underline"
+                          >
+                            {item.name}
+                          </Link>
+                        ))
+                        .reduce((prev, curr, idx) => {
+                          if (
+                            idx !== 0 &&
+                            idx === (printUrl.prerequisites?.length || 0) - 1
+                          )
+                            prev.push(" and ");
+                          else if (idx !== 0) prev.push(", ");
+                          prev.push(curr);
+                          return prev;
+                        }, [] as (JSX.Element | string)[])}
+                    </p>
                   ) : (
-                    <Icon as={IoPrintOutline} boxSize={6} />
+                    printUrl.error?.message ?? ""
                   )}
-                  Print Course Registration
-                </Button>
-              </PopoverTrigger>
-              {(printUrl.prerequisites || printUrl.error?.message) && (
-                <PopoverContent bg={"gray.400"} color="white">
-                  <PopoverArrow bg={"gray.400"} />
-                  <PopoverBody rounded={"md"}>
-                    {printUrl.prerequisites ? (
-                      <p>
-                        You must pay all outstanding fees to print your course
-                        registration:{" "}
-                        {printUrl.prerequisites
-                          .map((item) => (
-                            <Link
-                              key={`${item.id}-${item.transactionRef}`}
-                              as={NextLink}
-                              href={buildPaymentDetailUrl({
-                                id: item.id,
-                                trxRef: item.transactionRef,
-                                trxType: item.transactionType,
-                              })}
-                              textDecorationStyle="solid"
-                              textDecorationLine="underline"
-                            >
-                              {item.name}
-                            </Link>
-                          ))
-                          .reduce((prev, curr, idx) => {
-                            if (
-                              idx !== 0 &&
-                              idx === (printUrl.prerequisites?.length || 0) - 1
-                            )
-                              prev.push(" and ");
-                            else if (idx !== 0) prev.push(", ");
-                            prev.push(curr);
-                            return prev;
-                          }, [] as (JSX.Element | string)[])}
-                      </p>
-                    ) : (
-                      printUrl.error?.message ?? ""
-                    )}
-                  </PopoverBody>
-                </PopoverContent>
-              )}
-            </Popover>
-          )}
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </Popover>
         </Flex>
         <RadioButtonGroup
           display={["none", null, "flex"]}
@@ -253,20 +186,12 @@ const Courses: FC = () => {
           onChange={setView}
         />
       </Flex>
-      {inDeleteCourseView ? (
-        <DeleteCourseView
-          courseList={courses}
-          onDelete={onDelete}
-          view={view as "list" | "grid"}
-        />
-      ) : (
-        <CourseView
-          courseList={courses}
-          view={view as "list" | "grid"}
-          isLoading={registeredCoursesIsLoading}
-          error={registeredCoursesError as Error}
-        />
-      )}
+      <CourseView
+        courseList={courses}
+        view={view as "list" | "grid"}
+        isLoading={registeredCoursesIsLoading}
+        error={registeredCoursesError as Error}
+      />
     </>
   );
 };
