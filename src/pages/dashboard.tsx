c2888479bf3029c5b1e6e11aae12f75bb17cc476
Joshua Farayola <farayolajoshua@gmail.com>
2025-09-02 21:35:18 +0100

    fix: reorder CoursesCard and PaymentsCard in dashboard
    Ensure proper display order of course and payment cards.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <ProfileCard />
       <InitiateResultVerificationCard />
       <PraticumFormCard />
-      <CoursesCard />
       <PaymentsCard />
+      <CoursesCard />
     </>
   );
 }
